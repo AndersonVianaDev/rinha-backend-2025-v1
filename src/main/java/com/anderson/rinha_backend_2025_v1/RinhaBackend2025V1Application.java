8f7fdf2b5677fdd1fb7c98385833ed8097f97b39
AndersonDev <andersonp.viana.contato@gmail.com>
2025-07-19 13:27:07 -0300

    feat: project created

---
+++ src/main/java/com/anderson/rinha_backend_2025_v1/RinhaBackend2025V1Application.java
@@ -0,0 +1,13 @@
+package com.anderson.rinha_backend_2025_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackend2025V1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackend2025V1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/rinha_backend_2025_v1/RinhaBackend2025V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.rinha_backend_2025_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackend2025V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
